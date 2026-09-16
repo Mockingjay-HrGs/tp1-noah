6f18f771148db300f30a2c1d7393b50539cc72d8
Mockingjay <mockingjay@MacBook-Pro-de-Mockingjay.local>
2026-09-16 16:41:40 +0200

    test: la valeur du stock est arrondie au centime

--- inventaire/test_inventaire.py
+++ inventaire/test_inventaire.py
@@ -8,3 +8,10 @@ def test_valeur_stock_additionne_quantites_multipliees_par_prix():
     ]
 
     assert val(articles) == 39
+
+def test_valeur_stock_est_arrondie_au_centime():
+    articles = [
+        {"q": 3, "pu": 1.234},
+    ]
+
+    assert val(articles) == 3.70
