cbedc3868c781d4ffb773d669b52a02a004580ed
Mockingjay <mockingjay@MacBook-Pro-de-Mockingjay.local>
2026-09-16 11:31:00 +0200

    test: cent vingt minutes coutent quatre euros cinquante

--- kata_parking/test_parking.py
+++ kata_parking/test_parking.py
@@ -15,3 +15,6 @@ def test_stationnement_soixante_minutes_coute_un_euro_cinquante():
 
 def test_stationnement_soixante_et_une_minutes_coute_trois_euros():
     assert calculer_tarif(61) == 3
+
+def test_stationnement_cent_vingt_minutes_coute_quatre_euros_cinquante():
+    assert calculer_tarif(120) == 4.50
