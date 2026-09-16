ef71dfb9c6baf0268974e99da28c97139d21f770
Mockingjay <mockingjay@MacBook-Pro-de-Mockingjay.local>
2026-09-16 12:05:01 +0200

    green: offrir soixante minutes aux camions electriques branches

--- kata_parking/parking.py
+++ kata_parking/parking.py
@@ -1,10 +1,11 @@
 from math import ceil
 
 
-def calculer_tarif(minutes, abonne=False):
-    if minutes <= 30:
+def calculer_tarif(minutes, abonne=False, electrique_branche=False):
+    minutes_gratuites = 60 if electrique_branche else 30
+    if minutes <= minutes_gratuites:
         return 0
-    demi_heures_payantes = ceil((minutes - 30) / 30)
+    demi_heures_payantes = ceil((minutes - minutes_gratuites) / 30)
     tranches_journalieres = ceil(minutes / (24 * 60))
     plafond = tranches_journalieres * 18
     montant = min(demi_heures_payantes * 1.50, plafond)
