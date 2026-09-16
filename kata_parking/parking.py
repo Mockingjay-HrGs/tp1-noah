0ff28fa98d397651c81c4b68fd9dc9923c627983
Mockingjay <mockingjay@MacBook-Pro-de-Mockingjay.local>
2026-09-16 11:57:45 +0200

    green: appliquer une remise de quarante pour cent aux abonnes

--- kata_parking/parking.py
+++ kata_parking/parking.py
@@ -1,10 +1,13 @@
 from math import ceil
 
 
-def calculer_tarif(minutes):
+def calculer_tarif(minutes, abonne=False):
     if minutes <= 30:
         return 0
     demi_heures_payantes = ceil((minutes - 30) / 30)
     tranches_journalieres = ceil(minutes / (24 * 60))
     plafond = tranches_journalieres * 18
-    return min(demi_heures_payantes * 1.50, plafond)
+    montant = min(demi_heures_payantes * 1.50, plafond)
+    if abonne:
+        montant = round(montant * 0.60, 2)
+    return montant
