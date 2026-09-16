8cacb080a99f9da203f7758ad4ef02cfba20c1c5
Mockingjay <mockingjay@MacBook-Pro-de-Mockingjay.local>
2026-09-16 10:37:42 +0200

    test: trente minutes de stationnement sont gratuites

--- kata_parking/test_parking.py
+++ kata_parking/test_parking.py
@@ -3,3 +3,6 @@ from kata_parking.parking import calculer_tarif
 
 def test_stationnement_une_minute_est_gratuit():
     assert calculer_tarif(1) == 0
+
+def test_stationnement_trente_minutes_est_gratuit():
+    assert calculer_tarif(30) == 0
